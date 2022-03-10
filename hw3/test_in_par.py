01a11d9bef5c18dab236172786aa6573e59fc257
evankomp@uw.edu <evankomp@uw.edu>
2022-03-10 11:08:10 -0800

    add test of max bound

--- hw3/test_in_par.py
+++ hw3/test_in_par.py
@@ -43,3 +43,9 @@ if __name__ == '__main__':
         ]
     )
     Parallel(n_jobs=n_jobs)(delayed(test_agent_in_replicate)(*i) for i in inputs)
+    
+    class UCB_maxed(agents.UCB):
+        maxed = True
+
+    # do a test for maxing out
+    test_agent_in_replicate(UCB_maxed, dict(gamma = 2, beta_type='det', max_bound=True)
